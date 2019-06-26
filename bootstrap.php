970766d9a830ef7ca17e71c172102d0f8beed981
snipercool <sniper.cool@hotmail.com>
2019-06-26 23:38:58 +0200

    starting files

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+	// Start a new session
+	session_start();
+
+	// Autoload all classes inside classes folder
+	spl_autoload_register(function($class) {
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".php");
+	});
